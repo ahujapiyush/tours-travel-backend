a364b44d7f5e8f33761d05ce9bf13ab4774aad5e
Piyush Ahuja <piyushahuja@newetrix.com>
2026-03-01 09:14:18 +0530

    Fix: replace uuid (ESM-only) with crypto.randomUUID(); fix database.js knexfile issue

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,6 +1,6 @@
 const db = require('../config/database');
 const bcrypt = require('bcryptjs');
-const { v4: uuidv4 } = require('uuid');
+const { randomUUID: uuidv4 } = require('crypto');
 
 // GET /api/admin/customers?search=&page=&limit=
 exports.getCustomers = async (req, res, next) => {
--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,5 +1,5 @@
 const db = require('../config/database');
-const { v4: uuidv4 } = require('uuid');
+const { randomUUID: uuidv4 } = require('crypto');
 const notificationService = require('../services/notificationService');
 
 // Generate booking number
